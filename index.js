75bbc227e8e00c7e4ea8dcb3090180397a149d4c
Sajid <sajidmohammad460519@gmail.com>
2024-06-26 23:40:45 +0800

    able to update, add, get, and soon delete books, ngl backend http api calls to manipulate database pretty cool

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 
 import dotenv from "dotenv";
 
@@ -43,6 +43,65 @@ app.post("/books", async (request, response) => {
     response.status(500).send({ message: error.message });
   }
 });
+
+
+app.get('/books', async (request, response) => {
+  try {
+    const books = await Book.find({})
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    })
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message })
+  }
+})
+
+app.get('/books/:id', async (request, response) => {
+  try {
+
+    const { id } = request.params;
+
+    const book  = await Book.findById(id)
+
+    return response.status(200).json(book)
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message })
+  }
+})
+
+app.put('/books/:id', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishyear",
+      });
+    }
+
+      const { id } = request.params
+      const result = await Book.findByIdAndUpdate(id, request.body)
+
+      if (!result) {
+        return response.status(404).json({message: 'Book not found'})
+      }
+      return response.status(200).json({message: 'Book updated successfully'})
+
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message })
+  }
+})
+
 
 mongoose
   .connect(process.env.MONGODB_URI)
