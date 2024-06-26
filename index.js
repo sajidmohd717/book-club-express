25265669969dc16d14f2440d39b4e29007c46075
Sajid <sajidmohammad460519@gmail.com>
2024-06-26 21:13:32 +0800

    added schema and post method to add new book

--- index.js
+++ index.js
@@ -3,14 +3,45 @@ import express from "express";
 import dotenv from "dotenv";
 
 import mongoose from "mongoose";
+
+import { Book } from "./models/bookModel.js"; 
+
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3500; // If process.env.PORT is undefined, then it defaults to 3000
 const app = express();
 
+app.use(express.json()); //this is so that you can enter json inputs
+
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishyear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
