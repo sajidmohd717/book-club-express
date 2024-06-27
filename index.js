b3e34f57c60da0dec3c68c118fbf608f9faa8ac5
Sajid <sajidmohammad460519@gmail.com>
2024-06-27 23:34:03 +0800

    did till the cors policy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express, { response } from "express";
+import express from "express";
 
 import dotenv from "dotenv";
 
 import mongoose from "mongoose";
 
-import { Book } from "./models/bookModel.js"; 
+import { Book } from "./models/bookModel.js";
 
+import booksRoute from "./routes/booksRoute.js";
 
 dotenv.config();
 
@@ -14,94 +15,20 @@ const app = express();
 
 app.use(express.json()); //this is so that you can enter json inputs
 
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type'],
+// }));
+
+app.use(cors())
+
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack Tutorial");
 });
 
-app.post("/books", async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: "Send all required fields: title, author, publishyear",
-      });
-    }
-    const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      publishYear: request.body.publishYear,
-    };
-    const book = await Book.create(newBook);
-
-    return response.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-
-app.get('/books', async (request, response) => {
-  try {
-    const books = await Book.find({})
-
-    return response.status(200).json({
-      count: books.length,
-      data: books,
-    })
-
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message })
-  }
-})
-
-app.get('/books/:id', async (request, response) => {
-  try {
-
-    const { id } = request.params;
-
-    const book  = await Book.findById(id)
-
-    return response.status(200).json(book)
-
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message })
-  }
-})
-
-app.put('/books/:id', async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: "Send all required fields: title, author, publishyear",
-      });
-    }
-
-      const { id } = request.params
-      const result = await Book.findByIdAndUpdate(id, request.body)
-
-      if (!result) {
-        return response.status(404).json({message: 'Book not found'})
-      }
-      return response.status(200).json({message: 'Book updated successfully'})
-
-
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message })
-  }
-})
-
+app.use("/books", booksRoute);
 
 mongoose
   .connect(process.env.MONGODB_URI)
